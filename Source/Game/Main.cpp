9d4758af7ef1259941fcf3c25e95d29e4ed5e9c1
Cole-Lowery <CLowery@student.neumont.edu>
2025-07-02 09:15:02 -0600

    Add project files.

---
+++ Source/Game/Main.cpp
@@ -0,0 +1,10 @@
+#include "Core/Math.h"
+#include <iostream>
+
+#define NAME "Billy"
+
+int main() {
+	std::cout << NAME << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << viper::pi << std::endl;
+}
